e22eb5d94e7ff6c076c6d8a90374288bc1f89ec3
mohamadtaufikhidayat <mohamadtaufikhidayat65@gmail.com>
2022-04-17 15:40:29 +0700

    file soal ujian

--- src/NewGitHub/Soal1.java
+++ src/NewGitHub/Soal1.java
@@ -1,5 +1,39 @@
 package NewGitHub;
+import java.util.Scanner;
 
 public class Soal1 {
-
+	public int inputN(){
+		System.out.println("Masukan Nilai N:");
+		Scanner scan = new Scanner(System.in);
+		int n= scan.nextInt();
+		return n;
+	}
+	public int [][] isiArray(int n){
+		int [][]array= new int [2][n];
+		for(int i=0;i<2;i++) {
+			for(int j=0; j<n;j++) {
+				if(i==0) {
+					array[i][j]=2*j+1;
+				}else {
+					array[i][j]=2*n-1-2*j;
+				}
+			}
+		}
+		return array;
+		
+	}
+	public void cetakArray() {
+		int n=inputN();
+		int [][]array=isiArray(n);
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.printf("%3d",array[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) {
+		Soal1 run = new Soal1();
+		run.cetakArray();
+	}
 }
--- src/NewGitHub/Soal2.java
+++ src/NewGitHub/Soal2.java
@@ -1,5 +1,52 @@
 package NewGitHub;
+import java.util.Scanner;
 
 public class Soal2 {
+	public Scanner scan = new Scanner(System.in);
+//	public int inputN(){
+//		System.out.println("Masukan Nilai masukan uang Andi:");
+//		
+//		int n= scan.nextInt();
+//		return n;
+//	}
+	public int [][] ArrayItem(){
+		int arrayItem [][]=new int [2][3];
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<3;j++) {
+				if(i==0) {
+					System.out.println("Masukan hasukan harga kaca mata:"+(j+1));
+					arrayItem[i][j]=scan.nextInt();
+				}else {
+					System.out.println("Masukan hasukan harga baju:"+(j+1));
+					arrayItem[i][j]=scan.nextInt();
+				}
+			}
+		}
+		return arrayItem;
+	}
+	public void hasilSum() {
+		System.out.println("Masukan Nilai masukan uang Andi:");
+		int a=0;
+		int uangAndi= scan.nextInt();
+		int [][] arrayItem = ArrayItem();
+		int [][] arraySum = new int [3][3];
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				int	sum=Integer.valueOf(arrayItem[0][i])+Integer.valueOf(arrayItem[1][j]);
+				arraySum [i][j]=sum;
+				if(arraySum[i][j]<=uangAndi&&arraySum[i][j]>a) {
+				a=arraySum[i][j];
+				//System.out.println(arraySum[i][j]);
+				}
+			}
+		}
+		System.out.println(a);
+		
+	}
+	public static void main(String[] args) {
+		Soal2 run =new Soal2();
+		run.hasilSum();	
+	}
 
 }
+
